53dd38b9b93fea61352f4acfac8797f2b6293ddf
Ryan Felix <therjfelix@gmail.com>
2018-04-28 21:15:23 -0700

    Add utility functions
    * response.js contains some helpers for building Lambda responses
    
    * dynamodb.js contains a helper for performing DynamoDB actions

---
+++ workato-dashboard-api/src/lib/dynamodb.js
@@ -0,0 +1,8 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({ region: 'us-east-1'});
+
+export function perform(action, params) {
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    return dynamoDb[action](params).promise();
+}
